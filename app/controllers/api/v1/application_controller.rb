3f3565a6d073072a8f3c505a6a2bc776510892cb
macbook <taxiseguroalianza@gmail.com>
2016-06-01 17:38:56 -0500

    Agregadas rutas para api

---
+++ app/controllers/api/v1/application_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  
+  protect_from_forgery unless: -> { request.format.json? }
+  skip_before_filter :verify_authenticity_token
+end
