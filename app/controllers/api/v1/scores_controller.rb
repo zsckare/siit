39d6f81c186e9810db34626c72e072d384c7eb92
macbook <taxiseguroalianza@gmail.com>
2016-06-02 11:25:59 -0500

    Agreada ruta paa crear calificaciones

---
+++ app/controllers/api/v1/scores_controller.rb
@@ -0,0 +1,70 @@
+class Api::V1::ScoresController < Api::V1::ApplicationController
+  before_action :set_score, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @scores = Score.all
+  end
+
+  # GET /scores/1
+  # GET /scores/1.json
+  def show
+  end
+
+  # GET /scores/new
+  def new
+    @score = Score.new
+  end
+
+  # GET /scores/1/edit
+  def edit
+  end
+
+  # POST /scores
+  # POST /scores.json
+  def create
+    @score = Score.new(score_params)
+
+    
+      if @score.save
+        render json: @score 
+      else
+        render json: @score.errors
+      end
+    
+  end
+
+  # PATCH/PUT /scores/1
+  # PATCH/PUT /scores/1.json
+  def update
+    respond_to do |format|
+      if @score.update(score_params)
+        format.html { redirect_to @score, notice: 'Score was successfully updated.' }
+        format.json { render :show, status: :ok, location: @score }
+      else
+        format.html { render :edit }
+        format.json { render json: @score.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /scores/1
+  # DELETE /scores/1.json
+  def destroy
+    @score.destroy
+    respond_to do |format|
+      format.html { redirect_to scores_url, notice: 'Score was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_score
+      @score = Score.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def score_params
+      params.require(:score).permit(:score, :subject_id, :nocontrol)
+    end
+end
