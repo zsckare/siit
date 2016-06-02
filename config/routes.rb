ecbb5cacf75fcadd3ac8bac2d0b6ba90871c70f4
macbook <taxiseguroalianza@gmail.com>
2016-06-02 17:12:26 -0500

    Empece frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   resources :scores
   resources :students
   resources :subjects
   resources :teachers
-
+  root 'welcome#index'
   namespace :api ,defaults: {format: :json}do
     namespace :v1 do
         resources :subjects
